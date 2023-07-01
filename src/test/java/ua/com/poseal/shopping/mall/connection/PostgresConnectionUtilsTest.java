f96912b0b2301c06d2213dab9999f921f1c9368d
Sergiy Poddubka <poseal@gmail.com>
2023-07-01 13:28:50 +0300

    test

---
+++ src/test/java/ua/com/poseal/shopping/mall/connection/PostgresConnectionUtilsTest.java
@@ -0,0 +1,13 @@
+package ua.com.poseal.shopping.mall.connection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostgresConnectionUtilsTest {
+
+    @Test
+    void getConnection() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/ua/com/poseal/shopping/mall/util/ProductGeneratorTest.java
@@ -0,0 +1,18 @@
+package ua.com.poseal.shopping.mall.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductGeneratorTest {
+
+    @Test
+    void generateProduct() {
+        assertTrue(true);
+    }
+
+    @Test
+    void generateProducts() {
+        assertTrue(true);
+    }
+}
